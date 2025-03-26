ccdd6445af765470347834442a15ff25ca3a5883
MichaelGit202 <95455862+MichaelGit202@users.noreply.github.com>
2025-03-26 00:00:20 -0400

    move around stuff, actually added project folder, classified agents, agents talking to eachother

--- project/main.py
+++ project/main.py
@@ -8,7 +8,7 @@ sys.path.append(str(Path(__file__).parents[1]))
 
 
 from util.llm_utils import TemplateChat
-import util.llm_Agent_utils as llm_Utils
+from util.llm_Agent_utils import agent 
 
 # a dict of template files {"dungeon-master": {template file}}
     # Need to maintain concurrency across all template files, keeping each one up to date with what it needs to
@@ -26,59 +26,34 @@ def run_console_chat(seed, agents, **kwargs):
 
     while True:
         
-        response = llm_Utils.generate(agents["assistant1"])
-        llm_Utils.add_message(response, agents["assistant1"])
-        llm_Utils.add_message(response, agents["assistant2"])
+        response = agents["assistant1"].generate()
+        agents["assistant1"].add_message(response, agents["assistant1"])
+        agents["assistant1"].add_message(response, agents["assistant2"])
         print("assistant 1")
-        print(response)
+        print(response["message"]["message"]["content"])
 
         
-        response1 = llm_Utils.generate(agents["assistant2"])
+        response = agents["assistant2"].generate()
         print("assistant 2")
-        print(response1)
-        llm_Utils.add_message(response1, agents["assistant1"])
-        llm_Utils.add_message(response1, agents["assistant2"])
-  
-
-        #print("\n")
-        #print(agents["Dungeon_Master"])
-        #print("\n")
-        #print(agents["assistant2"])
-        #print("\n")
-
-
-       
-       
-       
-       # chat = TemplateChat.from_file(sign=seed, **kwargs)
-        
-
-        #chat_generator = chat.start_chat()
-        #print(next(chat_generator))
-
-        #while True: # you have to parameterize the exit condition for the agents here       
-        #    try:
-                # send user input
-                #message = chat_generator.send(input(''))
-         #       message -= chat_generator.send()
-         #   except StopIteration as e:
-          #      print(e)
-        
+        print(response["message"]["message"]["content"])
+        agents["assistant2"].add_message(response, agents["assistant1"])
+        agents["assistant2"].add_message(response, agents["assistant2"])
 
 
 
 if __name__ == "__main__":
     seed = '441_AI_Project'
     agents = {}
-    agent_list = os.listdir("project/agents")
 
+    print(os.listdir("441_project/project/agents"))
+    agent_list = os.listdir("441_project/project/agents")
+
+ 
     #populate agents list
-    for agent in agent_list:
-        with open("project/agents/" + agent, 'r') as file:
+    for agent_name in agent_list:
+        with open("441_project/project/agents/" + agent_name, 'r') as file:
             data = json.load(file)
-            agents[data["metadata"]["agent_name"]] = data
-            #agents[data["metadata"]["agent_name"]]["agent"] = data["metadata"]["agent_name"] #TODO this is scuffed
-            #agents[data["metadata"]["agent_name"]]["agent_template"] = data["agent_template"] # im more than certain 
-                                                                                                #that there is a way to load in th
+            agents[data["metadata"]["agent_name"]] = agent(data)
+                                                                                               #that there is a way to load in th
     run_console_chat(seed=seed, agents=agents)
     
--- project/util/llm_Agent_utils.py
+++ project/util/llm_Agent_utils.py
@@ -13,38 +13,36 @@ from collections import defaultdict
 ollama_seed = lambda x: int(str(int(hashlib.sha512(x.encode()).hexdigest(), 16))[:8])
 
 
-#function dedicated to prompting ollama
-def generate(agent):
-   
-    #ollama call with the template file
-    response = {}
-    #print(agent)
-    #print("WHAT THE FUCK +++++++++++++")
-    #print(agent["agent_template"])
-    #print("WHAT THE FUCK 222222222222222222222222222")
-    template = agent["agent_template"]
+class agent:
+    data = {}
+
+    def __init__(self, agent_info):  
+        self.data = agent_info
+    #function dedicated to prompting ollama
+    def generate(self):
     
-    response["message"] = ollama.chat(**template) 
-    response["origin"] = {}
-    response["origin"] = agent["metadata"]["agent_name"] 
-    #print(response)
-    return response
+        response = {}
+        response["message"] = ollama.chat(**self.data["agent_template"]) 
+        response["origin"] = {}
+        response["origin"] = self.data["metadata"]["agent_name"] 
+    
+        return response
 
 
-def add_message(msg, template):
-    #OK so here is what we are going to do
-    # to get these FUCKERS to talk to each other, we are going to add
-    # prompts from other agents will be appended as a USER prompt
+    def add_message(self, msg, agent):
+        #OK so here is what we are going to do
+        # to get these FUCKERS to talk to each other, we are going to add
+        # prompts from other agents will be appended as a USER prompt
+        #print(template)
+        #print(msg)
 
-   
-    #print(template)
-    #print(msg)
-     #TODO message manipulation is fucked right now ie msg["message"]["message"] <- what is causing this 
-    if(msg["origin"] == template["metadata"]["agent_name"]):
-        template["agent_template"]["messages"].append({"role" : msg["message"]["message"]["role"], "content" : msg["message"]["message"]["content"]})
-    else:
-        template["agent_template"]["messages"].append({"role" :"user", "content" : msg["message"]["message"]["content"]})
+         #TODO message manipulation is fucked right now ie msg["message"]["message"] <- what is causing this
+         # also this is eye vomit code 
+        if(msg["origin"] == agent.data["metadata"]["agent_name"]):
+            agent.data["agent_template"]["messages"].append({"role" : msg["message"]["message"]["role"], "content" : msg["message"]["message"]["content"]})
+        else:
+            agent.data["agent_template"]["messages"].append({"role" :"user", "content" : msg["message"]["message"]["content"]})
 
-def parse_generator_calls():
-    pass
+    #def parse_generator_calls():
+     #   pass
 
